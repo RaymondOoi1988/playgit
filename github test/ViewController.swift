9287a3c25c3fad786c1a158ca1e86332f44233a6
RaymondOoi <OoiEingWann@Oois-MacBook-Pro.local>
2017-08-11 16:50:14 +0800

    first commit

---
+++ github test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github test
+//
+//  Created by RaymondOoi on 11/08/2017.
+//  Copyright © 2017 RaymondOoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
